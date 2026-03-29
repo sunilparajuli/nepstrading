19652473abea1dc2ac04d9d152c172d3948301cd
sunil <sunilparajuli2002@gmail.com>
2026-03-29 22:26:27 +0545

    revert: remove is_admin middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,9 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\IsAdmin::class,
-        ]);
         $middleware->web(append: [
             \App\Http\Middleware\CheckMaintenanceMode::class,
         ]);
