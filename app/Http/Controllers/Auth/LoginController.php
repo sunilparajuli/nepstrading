6df5c71b77455b288b02de61dbe07c71b994c538
sunil <sunilparajuli2002@gmail.com>
2026-04-05 21:20:16 +0545

    Force admin login route redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -24,7 +24,7 @@ class LoginController extends Controller
             $request->session()->regenerate();
 
             if (Auth::user()->is_admin) {
-                return redirect()->intended(route('admin.dashboard'));
+                return redirect('/admin');
             }
 
             return redirect()->intended(route('home'));
